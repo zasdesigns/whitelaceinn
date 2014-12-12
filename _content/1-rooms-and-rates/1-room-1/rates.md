10cdd4f1d0fc10f7b5424d69b06a7d99f2d00bdf
zane-insight <zstatz@insightcreative.com>
2014-12-12 16:54:04 -0600

    Framework for Rooms & Rates List/Grid
    Started organizing each room in a sub-folder of /rooms-and-rates

--- _content/1-rooms-and-rates/1-room-1/rates.md
+++ _content/1-rooms-and-rates/1-room-1/rates.md
@@ -1,15 +1,7 @@
 ---
-title: Room 1
+title: Room 1 Rates
+_template: default
 ---
-
-### Room 1
-
-##### Door County Orchard:
-Captivating fabric in bright red and green trims all six windows in this sunny room. This picnic of colors creates a cheerful setting for the four-poster 1840’s canopy bed. The luxurious bathroom is in the adjoining tower.
-- Queen bed
-- Queen size whirlpool
-- TV-VCR-DVD
-- Wi-Fi
 
 **Per Night Rates October 26, 2014 thru June 20, 2015:**
 Sunday thru Thursday - $100, Friday/Saturday - $150
--- _themes/whitelace/js/whitelace.js
+++ _themes/whitelace/js/whitelace.js
@@ -1,4 +1,30 @@
-// Time to script the javas
-$(function() {
+jQuery(document).ready(function($){
+  $('.flexslider').flexslider({
+    controlNav: false,
+    directionNav: false
+  });
 
+  $('.flexslider-2').flexslider({
+    directionNav: false
+  });
+
+  $('.display-list').click(function(){
+    $('.room').each(function(){
+      $('.rooms.grid').removeClass('grid').addClass('list');
+      $(this).addClass('row-fluid');
+      $(this).find('> div').addClass('span12');
+      $(this).find('> div > div').addClass('span6');
+    });
+    return false;
+  });
+
+  $('.display-grid').click(function(){
+    $('.room').each(function(){
+      $('.rooms.list').removeClass('list').addClass('grid');
+      $(this).removeClass('row-fluid');
+      $(this).find('> div').removeClass('span12');
+      $(this).find('> div > div').removeClass('span6');
+    });
+    return false;
+  });
 });
